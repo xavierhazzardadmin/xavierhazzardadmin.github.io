24525b68cb7e6f9e5313a5db44085fb8d3d50e30
Xavier Hazzard <xavier@hazzard.org.uk>
2021-03-05 12:10:47 +0000

    Added responsive page loading, added 3 new Font-Awesome logos, git, github and npm. Broke the footer on the about page.

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-var copy = document.querySelector("#copy");
+//  main holds all the content
+const main = document.querySelector(
+    "main"
+);
+
+const copy = document.querySelector(
+    "#copy"
+);
+
+//  main fades in 400ms after page load
+window.onload = () => {
+    setTimeout(() => {
+        main.classList.add("fadeIn");
+    }, 300);
+
+    console.log("We have loaded");
+};
+
+//  controls year dynamically
+
 var dynamic_year = new Date().getFullYear();
 
 //This doesn't work
